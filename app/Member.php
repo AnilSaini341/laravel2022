4ca54062584730da3fa18008a3bcad60f2e88dce
AnilSaini <Anil.Saini@mashvirtual.in>
2022-04-19 15:04:20 +0530

    Accessor/Mutator Example

--- app/Member.php
+++ app/Member.php
@@ -10,10 +10,21 @@ class Member extends Model
     //public $timestamps=false; // False Timestamps
 
     // Accessor example 
+    //Modify data Before Display is called Accessor
     function getNameAttribute($value){
-        return ucfirst($value);
+       // return ucfirst($value);
+        return "Mr, ".$value;
     }
     function getAddressAttribute($value){
         return $value.", India";
+    }
+
+    //Mutator Example
+    // Modify data before save in database are called mutator
+    public function setNameAttribute($value){
+        return $this->attributes['name']= 'Mr. '.$value;
+    }
+    public function setAddressAttribute($value){
+        return $this->attributes['address']= $value.", India";
     }
 }
