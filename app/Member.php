17d683fd12dfaa7f1851f9921de31103fc1b766b
AnilSaini <Anil.Saini@mashvirtual.in>
2022-04-19 14:26:30 +0530

    Laravel Accessor

--- app/Member.php
+++ app/Member.php
@@ -8,4 +8,12 @@ class Member extends Model
 {
     //
     //public $timestamps=false; // False Timestamps
+
+    // Accessor example 
+    function getNameAttribute($value){
+        return ucfirst($value);
+    }
+    function getAddressAttribute($value){
+        return $value.", India";
+    }
 }
